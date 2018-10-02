b3365ee6b3ce5ea06a5e4ceee7df76f51943545f
vitaozim <victorhugocgodoi@gmail.com>
2018-10-02 17:03:28 -0300

    Só eu que trabalho nesse projeto 1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,19 @@
 jQuery(document).ready(function($) {
 	var $lista_mainposts = $('#primeiros-posts > ul');
+	var $lista_pinterest = $('#lateral aside.pinterest ul');
 
 	if ($lista_mainposts.length > 0) {
 		$lista_mainposts.masonry({
 			itemSelector: '.grid-item',
 			columnWidth: '.grid-sizer',
+			gutter: '.gutter-sizer',
+			percentPosition: true
+		});
+	}
+
+	if ($lista_pinterest.length > 0) {
+		$lista_pinterest.masonry({
+			itemSelector: '.grid-item',
 			gutter: '.gutter-sizer',
 			percentPosition: true
 		});
