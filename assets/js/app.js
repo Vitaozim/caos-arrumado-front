e040b33ccc7113bca69987e4b386b2255e391029
Victor Hugo <victorhugocgodoi@gmail.com>
2018-10-03 00:28:41 -0300

    Só eu que trabalho nesse projeto 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,4 +18,41 @@ jQuery(document).ready(function($) {
 			percentPosition: true
 		});
 	}
+
+
+	var $slider = $('#slider-principal');
+	if ($slider.length > 0) {
+		var $slides = $slider.find('section');
+		var currentSlide = 1;
+		var nSlides = $slides.length;
+		var $navBullets = $slider.find('.bullets button');
+		var $navNextPrev = $slider.find('.next-prev button');
+		var transitandoSlide = false;
+
+		var trocarSlider = function(futureSlide){
+			transitandoSlide = true;
+			$slides.eq(futureSlide-1).addClass('before-atual');
+			$slides.eq(currentSlide-1).addClass('deixando-atual')
+			.find('h2').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				$slides.eq(currentSlide-1).removeClass('db atual before-atual deixando-atual');
+				$slides.eq(futureSlide-1).addClass('db');
+				setTimeout(function(){
+					$slides.eq(futureSlide-1).addClass('atual');
+
+				}, 50);
+			});
+		}
+
+		setTimeout(function(){
+			console.log('foi');
+			trocarSlider(4);
+		}, 1000);
+	}
 });
+
+
+
+
+
+
+
